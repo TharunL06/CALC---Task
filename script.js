99e13c086f076d12e05e45064c0d41ee456cda40
Tharunlakshmanan <106159594+Tharunlakshmanan@users.noreply.github.com>
2023-11-22 13:17:24 +0530

    Mini calc

---
+++ script.js
@@ -0,0 +1,21 @@
+// // var for getting output
+// let outputScreen = document.getElementById("outputScreen");
+
+// function display(num) {
+//   // adding default value plus entering value
+//   outputScreen.value += num;
+// }
+// function calculate() {
+//   // if value is right calculate or else give alert
+//   try {
+//     outputScreen.value = eval(outputScreen.value);
+//   } catch (err) {
+//     alert("Invalid exp");
+//   }
+// }
+// function clear() {
+//   outputScreen.value = "";
+// }
+// function del() {
+//   outputScreen.value = outputScreen.value.slice(0, -1);
+// }
